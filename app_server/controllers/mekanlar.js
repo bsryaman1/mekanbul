5cb3ab1a926ef85c51ab2a99c91e665e635e6b0a
bsryaman1 <busrayaman70@gmail.com>
2022-10-13 19:43:36 +0300

    first commit

---
+++ app_server/controllers/mekanlar.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var router = express.Router();
+
+// slash gördüğün zaman function çalışır
+const anaSayfa = function (req, res, next) {
+  res.render("anasayfa", { title: "Ana Sayfa" });
+};
+
+
+const mekanBilgisi = function (req, res, next) {
+  res.render("mekanbilgisi", { title: "Mekan Bilgisi" });
+};
+
+
+const yorumEkle = function (req, res, next) {
+  res.render("yorumekle", { title: "Yorum Sayfası" });
+};
+
+
+
+module.exports={
+
+  anaSayfa,
+  mekanBilgisi,
+  yorumEkle,
+
+}
