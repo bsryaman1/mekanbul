bb94f9d4179490db051d73bc79477ce49c57ab63
bsryaman1 <busrayaman70@gmail.com>
2022-12-04 16:36:21 +0300

    Replit'e uygulama bağlandı

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,76 +1,101 @@
 var express = require("express");
 var router = express.Router();
-
-// slash gördüğün zaman function çalışır
-const anaSayfa = function (req, res, next) {
-  res.render("anasayfa", { 
-  "baslik":"Anasayfa",
-  "sayfaBaslik":{
-   "siteAd":"MekanBul",
-   "slogan":"Civardaki Mekanları Keşfet!"},
-  "mekanlar": [
-    {
-      "ad":"Starbucks",
-      "adres":"Centrum Garden AVM",
-      "puan":"4",
-      "imkanlar":["Dünya Kahveleri","Kekler","Pastalar"],
-      "mesafe":"10km"
-
+const axios=require("axios");
+var apiSecenekleri={
+  //sunucu: "http://localhost:3000",
+  sunucu: "https://mekanbul-1.busrayaman.repl.co",
+  apiYolu: "/api/mekanlar/"
+}
+var mesafeyiFormatla=function(mesafe){
+  var yeniMesafe,birim;
+  if(mesafe>1){
+    yeniMesafe=parseFloat(mesafe).toFixed(1);
+    birim=" km";
+  }
+  else{
+    yeniMesafe=parseInt(mesafe*1000,10);
+    birim=" m";
+  }
+  return yeniMesafe+birim;
+  }
+var anasayfaOlustur=function(res,mekanListesi){
+  var mesaj;
+  if(!(mekanListesi instanceof Array)){
+    mesaj="API Hatası: Bir şeyler ters gitti";
+    mekanListesi=[];
+  }else{
+    if(!mekanListesi.length){
+      mesaj="Civarda hiç mekan yok!";
     }
-    ,
-    {
-      "ad":"Gloria Jeans",
-      "adres":"Sdü Doğu Kampüsü",
-      "puan":"3",
-      "imkanlar":["Kahve","Çay","Tatlı"],
-      "mesafe":"5km"
-    }
-  ]
-});
+
+  }
+  res.render("anasayfa",{
+    "baslik": "Anasayfa",
+    "sayfaBaslik": {
+      "siteAd":"Mekanbul",
+      "slogan":"Civardaki Mekanları Keşfet!"
+    },
+    "mekanlar":mekanListesi,
+    "mesaj":mesaj
+  });
+}
+
+var detaySayfasiOlustur=function(res,mekanDetaylari){
+  mekanDetaylari.koordinat={
+    "enlem":mekanDetaylari.koordinat[0],
+    "boylam":mekanDetaylari.koordinat[1]
+  }
+  res.render('mekanbilgisi',
+  {
+    mekanBaslik:mekanDetaylari.ad,
+    mekanDetay:mekanDetaylari
+  });
+}
+
+var hataGonder=function(res,hata){
+  var mesaj;
+  if(hata.response.status==404){
+    mesaj="404, Sayfa Bulunamadı!";
+  } else{
+    mesaj=hata.response.status+"hatası";
+  }
+  res.status(hata.response.status);
+  res.render('error',{
+    "mesaj":mesaj
+  });
 };
 
-
-
-
-const mekanBilgisi = function (req, res, next) {
-  res.render("mekanbilgisi",
-  {
-    "baslik":"Mekan Bilgisi",
-    "mekanBaslik":"Starbucks",
-    "mekanDetay":{
-      "ad":"Starbucks",
-      "adres":"Centrum Garden AVM",
-      "puan":"4",
-      "saatler":[
-      {
-        "gunler":"Pazartesi-Cuma",
-        "acilis":"09.00",
-        "kapanis":"23.00",
-        "kapali": false
-      },
-      {
-        "gunler":"Cumartesi-Pazar",
-        "acilis":"10.00",
-        "kapanis":"23.00",
-        "kapali": false
-      }
-      ],
-      "imkanlar":["Dünya Kahveleri","Kekler","Pastalar"],
-      "koordinatlar":{
-        "enlem":"37.7",
-        "boylam":"30.5"
-      },
-      "yorumlar":[{
-        "yorumYapan":"Levent Habip",
-        "puan":"4",
-        "tarih":"20 Ekim 2022",
-        "yorumMetni":"Tavla varsa güzeldir."
-      }
-    ]  
+const anaSayfa = function (req, res) {
+  axios.get(apiSecenekleri.sunucu+apiSecenekleri.apiYolu,{
+    params:{
+      enlem: req.query.enlem,
+      boylam: req.query.boylam
     }
-  }
-  
-  );
+  }).then(function(response){
+    var i,mekanlar;
+    mekanlar=response.data;
+    for(i=0;i<mekanlar.length;i++){
+      mekanlar[i].mesafe=mesafeyiFormatla(mekanlar[i].mesafe);
+    }
+    anasayfaOlustur(res,mekanlar);
+  }).catch(function(hata){
+    anasayfaOlustur(res,hata);
+  });
+}
+
+
+
+
+const mekanBilgisi = function (req, res) {
+  axios
+    .get(apiSecenekleri.sunucu+apiSecenekleri.apiYolu+req.params.mekanid)
+    .then(function(response){
+      detaySayfasiOlustur(res,response.data);
+    })
+    .catch(function(hata){
+      hataGonder(res,hata);
+    });
+    
 };
 
 
